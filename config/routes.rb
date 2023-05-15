da36a65c7878ba89d2972c5304f8fe38835978a2
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2023-05-15 07:09:27 +0100

    barebone with devise for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # get 'posts/index'
+  # get 'posts/new'
+  # get 'posts/create'
+  root "posts#index"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resources :posts, only: [:new, :create, :index]
 end
